18176058f84f7c6b5bed62297464921af5a84785
Christine McClure <camcclure@gmail.com>
2012-02-02 11:34:55 -0600

    corrected nav for header and home page

--- index.php
+++ index.php
@@ -12,18 +12,18 @@
   <h2 id="photoHeader">photography</h2>
   <nav id="galleries">
   	<ul>
-    	<li><a href="fine-art.php"><p>fine art</p><img src="/img/art.jpg" alt="Photo of tulip curved so that it appears to be drooping over another closeup photo of a person's eye." /></a></li>
-    	<li><a href="portraits.php"><p>portraiture</p><img src="/img/portraits.jpg" alt="Photo of a stwo-year-old girl eating a cookie with powdered sugar on her face." /></a></li>
-    	<li><a href="events.php"><p>events</p><img src="/img/events.jpg" alt="Photo of a speaker from the 2010 Literature For All of Us, Honor Thy Mother Luncheon" /></a></li>    
-    	<li><a href="travel.php"><p>travel</p><img src="/img/travel.jpg" alt="Closeup photo of a flower in the Boquete national Forest, Panama." /></a></li>    
+    	<li><a href="/fine-art"><p>fine art</p><img src="/img/art.jpg" alt="Photo of tulip curved so that it appears to be drooping over another closeup photo of a person's eye." /></a></li>
+    	<li><a href="/portraits"><p>portraiture</p><img src="/img/portraits.jpg" alt="Photo of a stwo-year-old girl eating a cookie with powdered sugar on her face." /></a></li>
+    	<li><a href="/events"><p>events</p><img src="/img/events.jpg" alt="Photo of a speaker from the 2010 Literature For All of Us, Honor Thy Mother Luncheon" /></a></li>    
+    	<li><a href="/travel"><p>travel</p><img src="/img/travel.jpg" alt="Closeup photo of a flower in the Boquete national Forest, Panama." /></a></li>    
     </ul>
   </nav>
   <p id="photoIntro">I've been fascinated with photography ever since my childhood in the seventies, where I staged a photo shoot using a velour bathrobe as a backdrop, ceramic dog figurines as models, and my trusty little 110 plastic camera.</p>
   <nav id="photo">
   	<ul>
-    	<li><a href="about.php">about</a></li>
-    	<li><a href="pricing.php">pricing</a></li>
-    	<li><a href="contact.php">contact</a></li>
+    	<li><a href="/about">about</a></li>
+    	<li><a href="/pricing">pricing</a></li>
+    	<li><a href="/contact">contact</a></li>
     </ul>
   </nav>
 
@@ -32,10 +32,10 @@
 	<p id="libIntro">I'm currently the Digital Services Librarian at a private university in Chicago, where I make all of our various technologies talk to each other. My goal is to provide a rich online experience for students that doesn't take a library degree to navigate.</p>
   <nav id="lib">
   	<ul>
-    		<li><a href="resume.php">resum&eacute;</a></li>
-    		<li><a href="portfolio.php">portfolio</a></li>
-    		<li><a href="the-lab.php">the lab</a></li>
-    		<li><a href="contact.php">contact</a></li>
+    		<li><a href="/resume">resum&eacute;</a></li>
+    		<li><a href="/portfolio">portfolio</a></li>
+    		<li><a href="/the-lab">the lab</a></li>
+    		<li><a href="/contact">contact</a></li>
     </ul>
   </nav>
 	<?php include 'includes/footer.html'; ?>
