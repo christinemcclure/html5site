12c44812d533f5504ed89ff9d6c88b4796bf29c9
Christine McClure <camcclure@gmail.com>
2012-02-10 14:30:04 -0600

    change art page to photoswipe

--- fine-art/index.php
+++ fine-art/index.php
@@ -1,5 +1,9 @@
 <?php include '../includes/head-stub.html' ?>
 
+	<link href="../js/photoSwipe/photoswipe.css" type="text/css" rel="stylesheet" />
+	<script type="text/javascript" src="../js/photoSwipe/lib/klass.min.js"></script>
+	<script type="text/javascript" src="../js/photoSwipe/code.photoswipe-3.0.4.min.js"></script>	
+
   <title>Christine McClure -- Fine Art Photography</title>
 </head>
 
@@ -17,36 +21,110 @@
 
     <h2><strong>Focus</strong> (Religion as barrier)</h2>
     <h3>Jerusalem, Israel</h3>
-    <ul>
-      <li><img src="/img/fineart/focus-1.jpg" alt="Circular photo. Top half is out-of-focus dark shapes. Bottom half is the top portion of a gate (barrier) in up-close detail" /></li>
-      <li><img src="/img/fineart/focus-2.jpg" alt="Circular photo. Top half is slightly out-of-focus dark shapes. Bottom half is slightly out-of-focus gate (barrier)." /></li>
-      <li><img src="/img/fineart/focus-3.jpg" alt="Circular photo. Top half shows orthodox Jewish men praying at the Kotel (Wailing Wall) in Jerusalem. Bottom half is out-of-focus gate (barrier)." /></li>
-    </ul>
-  
+   <div class="gallery">
+	    <div id="gallery1" class="photoSwipe"></div>
+   </div>
+
     <h2><strong>Macro</strong> (Life in the details)</h2>
     <h3>Chicago, Illinois</h3>
-    <ul>
-      <li><img src="/img/fineart/macro-1.jpg" alt="Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois." /></li>
-      <li><img src="/img/fineart/macro-2.jpg" alt="Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois." /></li>
-      <li><img src="/img/fineart/macro-3.jpg" alt="Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois." /></li>    
-    </ul>
-  
+
+   <div class="gallery">
+	    <div id="gallery2" class="photoSwipe"></div>
+   </div>
+
     <h2><strong>Persephone's Garden</strong> (How did she pass the time?)</h2>
     <h3>Chicago, Illinois</h3>
-		<ul>
-      <li><img src="/img/fineart/persephone-1.jpg" alt="Digitally-manipulated image of cement planters with overly-large blue plant erupting from one of them." /></li>
-      <li><img src="/img/fineart/persephone-2.jpg" alt="Digitally-manipulated image of a decrepit Chicago mansion with overly large cacti as the front lawn." /></li>
-      <li><img src="/img/fineart/persephone-3.jpg" alt="Digitally-manipulated image of a large fern-like plant in grayscale growing out of large red rocks (magnified pebbles)." /></li>    
-    </ul>
+   <div class="gallery">
+	    <div id="gallery3" class="photoSwipe"></div>
+   </div>
+
 
 </section>
 
 <?php include '../includes/footer.html'; ?>
 </div>
+	<script type="text/javascript">
+		(function(window, Util, PhotoSwipe){			
+			Util.Events.domReady(function(e){				
+				var gallery1;		
+				var gallery2;		
+				var gallery3;
+				gallery1 = PhotoSwipe.attach(
+					[
+						{ url: '/img/fineart/focus-1.jpg', 
+								alt:'Circular photo. Top half is out-of-focus dark shapes. Bottom half is the top portion of a gate (barrier) in up-close detail.'},
+						{ url: '/img/fineart/focus-2.jpg', 
+								alt:'Circular photo. Top half is slightly out-of-focus dark shapes. Bottom half is slightly out-of-focus gate (barrier).'},
+						{ url: '/img/fineart/focus-3.jpg', 
+								alt:'Circular photo. Top half shows orthodox Jewish men praying at the Kotel (Wailing Wall) in Jerusalem. Bottom half is out-of-focus gate (barrier).'}
+					],
+					{
+						target: window.document.querySelectorAll('#gallery1')[0],
+						preventHide: true,
+						captionAndToolbarShowEmptyCaptions: false,
+						getImageSource: function(obj){
+							return obj.url;
+						},
+						getImageCaption: function(obj){
+							return obj.caption;
+						}
+					}
+				);
+				gallery2 = PhotoSwipe.attach(
+					[
+						{ url: '/img/fineart/macro-1.jpg', 
+								alt:'Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois.'},
+						{ url: '/img/fineart/macro-2.jpg', 
+								alt:'Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois.'},
+						{ url: '/img/fineart/macro-3.jpg', 
+								alt:'Circular photo of a succulent plant (unknown species) taken at the Garfield Park Conservatory in Chicago, Illinois.'}
+					],
+					{
+						target: window.document.querySelectorAll('#gallery2')[0],
+						preventHide: true,
+						captionAndToolbarShowEmptyCaptions: false,
+						getImageSource: function(obj){
+							return obj.url;
+						},
+						getImageCaption: function(obj){
+							return obj.caption;
+						}
+					}
+				);				
+				gallery3 = PhotoSwipe.attach(
+					[
+						{ url: '/img/fineart/persephone-1.jpg', 
+								alt:'Digitally-manipulated image of cement planters with overly-large blue plant erupting from one of them.'},
+						{ url: '/img/fineart/persephone-2.jpg', 
+								alt:'Digitally-manipulated image of a decrepit Chicago mansion with overly large cacti as the front lawn.'},
+						{ url: '/img/fineart/persephone-3.jpg', 
+								alt:'Digitally-manipulated image of a large fern-like plant in grayscale growing out of large red rocks (magnified pebbles).'}
+					],
+					{
+						target: window.document.querySelectorAll('#gallery3')[0],
+						preventHide: true,
+						captionAndToolbarShowEmptyCaptions: false,
+						getImageSource: function(obj){
+							return obj.url;
+						},
+						getImageCaption: function(obj){
+							return obj.caption;
+						}
+					}
+				);				
+				gallery1.show(0);				
+				gallery2.show(0);				
+				gallery3.show(0);				
+			});						
+		}(window, window.Code.Util, window.Code.PhotoSwipe));		
+	</script>
+  
+
 <script type="text/javascript">
 	$(document).ready(function() {
 		// set initial nav state for this page
-			setNav("photo", "art");			
+			setNav("photo", "art");		
+			setAllGalleryHeights('.photoSwipe');			
 	});
 </script>
 </body>
