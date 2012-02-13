3730965205ea9b49eb5ac1b4ff8e77aab6ee8a28
Christine McClure <camcclure@gmail.com>
2012-02-13 16:48:50 -0600

    added columns for pricing

--- pricing/index.php
+++ pricing/index.php
@@ -12,18 +12,27 @@
 <section>
 	<h1 class="top">Event Photography</h1>
 
-	<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
-
-	<p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be ordered onlineat an additional cost.</p>
-</section>
+	<ul class="twoColumnBlock">
+  <li>
+		<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
+  </li>
+    <li>
+      <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be ordered onlineat an additional cost.</p>
+    </li>
+  </ul>
 
 <section>
 	<h1>Portraiture</h1>
 
-	<p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
-  
-	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.
+	<ul class="twoColumnBlock">
+  	<li>
+			<p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
+  	</li>
+		<li>
+    	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.
 After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be ordered online at an additional cost.</p>
+		</li>
+	</ul>
 </section>
 
 <?php include '../includes/footer.html'; ?>
