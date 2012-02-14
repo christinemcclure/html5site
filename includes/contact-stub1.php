100243081709ca6ce1a583c76fc339ee499e54ff
Christine McClure <camcclure@gmail.com>
2012-02-14 10:22:10 -0600

    changed contact include to two stubs to allow for return to correct form

--- includes/contact-stub1.php
+++ includes/contact-stub1.php
@@ -72,16 +72,3 @@ if (isset($_POST['submitted'])) {
   
 		  <?php echo $feedback; ?>  
       	<div id="showContactForm" class="<?php echo $cssclass; ?>"> <!--showForm to start-->	
-	      <form method="post" action="/contactLib" id="contactForm">
-    	  <p><input type="text" name="name" id="name" placeholder="Name" /></p>
-  	    <p><input type="email" name="email" id="email" placeholder="E-mail address"/></p>
-  	    <p><input type="text" name="phone" id="phone" placeholder="Phone number" /></p>
-  	    <p><input type="text" name="message" id="message" placeholder="Your message here" /></p>
-	      <p><input class="button" type="submit" name="submit" value="Send" id="submitButton" />
-      <input type="hidden" name="submitted" value="TRUE" /></p>
-  	  </form>
-    </div>
-
-</section>
-<?php include 'footer.html'; ?>
-</div>
---
+++ includes/contact-stub2.php
@@ -0,0 +1,13 @@
+
+    	  <p><input type="text" name="name" id="name" placeholder="Name" /></p>
+  	    <p><input type="email" name="email" id="email" placeholder="E-mail address"/></p>
+  	    <p><input type="text" name="phone" id="phone" placeholder="Phone number" /></p>
+  	    <p><input type="text" name="message" id="message" placeholder="Your message here" /></p>
+	      <p><input class="button" type="submit" name="submit" value="Send" id="submitButton" />
+      <input type="hidden" name="submitted" value="TRUE" /></p>
+  	  </form>
+    </div>
+
+</section>
+<?php include 'footer.html'; ?>
+</div>
