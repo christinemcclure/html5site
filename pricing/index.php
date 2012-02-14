7980b3bf230e34e00ee6acec5902eeddfcdc9f99
Christine McClure <camcclure@gmail.com>
2012-02-14 11:50:19 -0600

    added color to a tags, smugmug links and more contact styling

--- pricing/index.php
+++ pricing/index.php
@@ -17,7 +17,7 @@
 		<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
   </li>
     <li>
-      <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be ordered onlineat an additional cost.</p>
+      <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
     </li>
   </ul>
 
@@ -30,7 +30,7 @@
   	</li>
 		<li>
     	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.
-After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be ordered online at an additional cost.</p>
+After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
 		</li>
 	</ul>
 </section>
