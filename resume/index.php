e1dd9fd907925e38ebd7f17dc9eadca659b7a6eb
Christine McClure <camcclure@gmail.com>
2012-02-15 16:30:30 -0600

    stopping for day. stillworking on resume structure

--- resume/index.php
+++ resume/index.php
@@ -16,7 +16,7 @@
 
     <h3>Digital Services Librarian</h3>
 	    <p>Illinois Institute of Technology</p>
-				<ul class="oneColumnBlock">
+				<ul class="twoColumnBlock">
         	<li>
             <h4>Web Design/Development</h4>
               <ul>
@@ -44,135 +44,166 @@
               </ul>
           </li>
 		</ul>
+    
     <h3>Reference Librarian</h3>
 	    <p>Illinois Institute of Technology</p>
 
-      <h4>Subject Liaison / Instruction</h4>
-        <ul class="columnBlock">
-	        <li>Supported research at Stuart School of Business and the Institute of Design as subject liaison. </li>
-      </ul>
-
-    <h4>Instruction</h4>
-    <ul class="columnBlock">
-    <li><em>Designed instruction sessions</em> in conjunction with class projects.</li>
-    </ul>
-
-    <h4>Reference Assistance</h4>
-      <ul class="columnBlock">
-	      <li>Provided assistance for all method of inquiries while staffing the reference desk, including short instruction sessions.</li>	     
-      </ul>
-
-    <h4>Collection Development</h4>
-      <ul class="columnBlock">
-        <li>Researched and purchased items for monograph resources for business and design subject areas.Also made recommendations for serial purchases in these areas.</li>
-        <li>Relocated and weeded items to maintain library collection for business and design areas.</li>
-      </ul>
-
+			<ul class="twoColumnBlock">
+				<li>
+          <h4>Subject Liaison / Instruction</h4>
+            <ul>
+              <li>Supported research at Stuart School of Business and the Institute of Design as subject liaison. </li>
+          </ul>
+				</li>
+        <li>
+          <h4>Instruction</h4>
+          <ul>
+          <li><em>Designed instruction sessions</em> in conjunction with class projects.</li>
+          </ul>
+				</li>
+        <li>
+          <h4>Reference Assistance</h4>
+            <ul>
+              <li>Provided assistance for all method of inquiries while staffing the reference desk, including short instruction sessions.</li>	     
+            </ul>
+				</li>
+        <li>
+          <h4>Collection Development</h4>
+            <ul>
+              <li>Researched and purchased items for monograph resources for business and design subject areas.Also made recommendations for serial purchases in these areas.</li>
+              <li>Relocated and weeded items to maintain library collection for business and design areas.</li>
+            </ul>
+				</li>
+			</ul>
+      
     <h3>Implementation Librarian </h3>
       <p>Ex Libris Group</p>
-      
-      <h4>Customer Management</h4>
-        <ul class="columnBlock">
-          <li>Coordinated software installations and upgrades for the Primo discovery and delivery application. </li>
-          <li>Communicated issues and requests to the members of the Primo team for resolution and possible 
-          inclusion in future product releases.</li>
-          <li>Configured environment and and provided <em>first line support</em> during the implementation cycle.</li>
-        </ul>
+			<ul class="twoColumnBlock">
+       	<li>      
+          <h4>Customer Management</h4>
+            <ul>
+              <li>Coordinated software installations and upgrades for the Primo discovery and delivery application. </li>
+              <li>Communicated issues and requests to the members of the Primo team for resolution and possible 
+              inclusion in future product releases.</li>
+              <li>Configured environment and and provided <em>first line support</em> during the implementation cycle.</li>
+            </ul>
+         </li>
+         <li>
+          <h4>Data Analysis</h4>
+            <ul>
+              <li>Wrote data map customizations to correct for a variety of display issues.</li>
+              <li>Provided analysis of future software needs and current defects to address.</li>
+            </ul>
+         </li>
+         <li>
+          <h4>Training/Documentation</h4>
+            <ul>
+              <li>Developed customized web-based and face-to-face instructional programs using active learning techniques. </li>
+              <li>Reviewed end user documentation for clarity and accuracy. </li>
+            </ul>
+         </li>
+       </ul>
 
-      <h4>Data Analysis</h4>
-        <ul class="columnBlock">
-          <li>Wrote data map customizations to correct for a variety of display issues.</li>
-          <li>Provided analysis of future software needs and current defects to address.</li>
-        </ul>
-
-      <h4>Training/Documentation</h4>
-        <ul class="columnBlock">
-          <li>Developed customized web-based and face-to-face instructional programs using active learning techniques. </li>
-          <li>Reviewed end user documentation for clarity and accuracy. </li>
-        </ul>
-      
     <h3>Quality Assurance Analyst</h3>
 	    <p>Endeavor Information Systems</p>
-
-      <h4>Software Testing</h4>
-        <ul class="columnBlock">
-          <li>System tested Voyager ILS and Meridian/ENCompass digital content management products.</li> 
-          <li>Maintained test environments, including creating test data.</li>
-        </ul>
+			<ul class="twoColumnBlock">
+       	<li>      
+          <h4>Software Testing</h4>
+            <ul>
+              <li>System tested Voyager ILS and Meridian/ENCompass digital content management products.</li> 
+              <li>Maintained test environments, including creating test data.</li>
+            </ul>
+        </li>
+        <li>
+          <h4>Field Testing</h4>
+            <ul>
+              <li>Organized customer test sites and testing process.</li> 
+              <li>Provided customer support and acted as liaison between customers and development staff.</li>
+            </ul>
+				</li>
+        <li>      
+          <h4>Test Automation/Training</h4>
+            <ul>
+              <li>Improved test automation methods, <em>refactored code into functional libraries</em> and cross-trained team members in 
+              programming methodologies.</li>
+              <li>Performed tool analysis comparisons for future performance testing goals.</li>
+            </ul>
+				</li>
+			</ul>
       
-      <h4>Field Testing</h4>
-        <ul class="columnBlock">
-          <li>Organized customer test sites and testing process.</li> 
-          <li>Provided customer support and acted as liaison between customers and development staff.</li>
-        </ul>
-      
-      <h4>Test Automation/Training</h4>
-        <ul class="columnBlock">
-          <li>Improved test automation methods, <em>refactored code into functional libraries</em> and cross-trained team members in 
-          programming methodologies.</li>
-          <li>Performed tool analysis comparisons for future performance testing goals.</li>
-        </ul>
-
     <h3>Senior Technical Consultant</h3>
-      <p>11th Hour, Inc.</p>
-      
-      <h4>Software Testing</h4>
-        <ul class="columnBlock">
-          <li>Developed structured testing environments and managed people and processes involved. </li>
-          <li>Designed and conducted performance tests to discover application latencies before release.</li>
-      </ul>
-      
-      <h4>Technical Writing</h4>
-        <ul class="columnBlock">
-	        <li>Created in-house user manuals and training documents.</li>
-        </ul>
-      
-      <h4>Training/Supervision</h4>
-        <ul class="columnBlock">
-          <li>Conducted standard training classes as well developed customized classes.</li>
-          <li>Co-wrote and presented a <em>conference paper for an audience of 100+ attendees</em>.</li>
-          <li>Supervised teams of 5-10 people, directing activities and outlining work.</li>
-        </ul>
-      
-      <h4>Website Design</h4>
-        <ul class="columnBlock">
-	        <li>Designed, developed and maintained two company websites.</li>
-        </ul>
-      
-      <h4>Graphic Design</h4>
-        <ul class="columnBlock">
-	        <li>Designed marketing materials such as <em>logos, business cards and brochures</em>.</li>
-        </ul>
+      <p>11th Hour, Inc.</p>    
+			<ul class="twoColumnBlock">
+       	<li>      
+          <h4>Software Testing</h4>
+          <ul>
+            <li>Developed structured testing environments and managed people and processes involved. </li>
+            <li>Designed and conducted performance tests to discover application latencies before release.</li>
+	        </ul>
+  			</li>
+        <li>    
+          <h4>Technical Writing</h4>
+            <ul>
+              <li>Created in-house user manuals and training documents.</li>
+            </ul>
+				</li>
+        <li>      
+          <h4>Training/Supervision</h4>
+            <ul>
+              <li>Conducted standard training classes as well developed customized classes.</li>
+              <li>Co-wrote and presented a <em>conference paper for an audience of 100+ attendees</em>.</li>
+              <li>Supervised teams of 5-10 people, directing activities and outlining work.</li>
+            </ul>
+				</li>     
+				<li>         
+          <h4>Website Design</h4>
+            <ul>
+              <li>Designed, developed and maintained two company websites.</li>
+            </ul>
+				</li>
+        <li>      
+          <h4>Graphic Design</h4>
+            <ul>
+              <li>Designed marketing materials such as <em>logos, business cards and brochures</em>.</li>
+            </ul>
+				</li>
+       </ul>
 
   <h2>Technical Skills</h2>
     <h3>Software</h3>
-      <h4>Library Systems</h4>
-        <ul class="columnBlock">
-	        <li>Primo Discovery + Delivery, Voyager ILS, Meridian ERM, SFX, MetaLib federated search, ContentDM, ENCompass digital repository</li>
-        </ul>
-
-      <h4>Software Development</h4>
-        <ul class="columnBlock">
-	        <li>Dreamweaver, BBEdit, NetBeans, <em>GitHub</em></li>
-        </ul>
-
-      <h4>General</h4>
-        <ul class="columnBlock">
-	        <li>Microsoft Office (including macro and VBA programming)</li>
-        </ul>
-      
-      <h4>Graphic Design</h4>
-        <ul class="columnBlock">
-	        <li>Adobe Creative Suite (Photoshop, Illustrator, InDesign)</li>
-        </ul>
-
+			<ul class="twoColumnBlock">
+       	<li>      
+          <h4>Library Systems</h4>
+            <ul>
+              <li>Primo Discovery + Delivery, Voyager ILS, Meridian ERM, SFX, MetaLib federated search, ContentDM, ENCompass digital repository</li>
+            </ul>
+         </li>
+				<li>
+          <h4>Software Development</h4>
+            <ul>
+              <li>Dreamweaver, BBEdit, NetBeans, <em>Git</em></li>
+            </ul>
+				</li>
+        <li>
+          <h4>General</h4>
+            <ul>
+              <li>Microsoft Office (including macro and VBA programming)</li>
+            </ul>
+				</li>
+        <li>      
+          <h4>Graphic Design</h4>
+            <ul>
+              <li>Adobe Creative Suite (Photoshop, Illustrator, InDesign)</li>
+            </ul>
+				</li>
+       </ul>
     <h3>Operating Systems</h3>
-      <ul class="columnBlock">
+      <ul>
 	      <li>Windows 7/XP/NT, UNIX, Linux, Mac OSX</li>
       </ul>
 
     <h3>Programming Languages</h3>
-      <ul class="columnBlock">
+      <ul>
 	      <li>HTML, PHP, <em>JavaScript/jQuery, Ruby on Rails,</em> MySQL, Perl</li>
       </ul>
 
@@ -190,7 +221,7 @@
     </ul>
  
   <h2>Activities / Interests</h2>
-    <ul class="columnBlock">
+    <ul>
       <li>Active member of ALA, RUSA, and LITA</li>
       <li><em>Photography</em> (fine art, portraiture and event photography)</li>
       <li>Graphic design</li>
