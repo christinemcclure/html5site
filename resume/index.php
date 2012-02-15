dc5928b5de640907a954cd9fcee02d1bd06af8f0
Christine McClure <camcclure@gmail.com>
2012-02-15 11:25:10 -0600

    starting resume styling. first section done

--- resume/index.php
+++ resume/index.php
@@ -10,59 +10,68 @@
 <?php include '../includes/header.html'; ?>
 <?php include '../includes/nav.html'; ?>
 <section>
-  <h1 class="top">Experience</h1>
-    <h2>Digital Services Librarian</h2>
+  <h1 class="top">Resum&eacute;</h1>
+
+  <h2 class="top">Experience</h2>
+
+    <h3>Digital Services Librarian</h3>
+	    <p>Illinois Institute of Technology</p>
+				<ul class="oneColumnBlock">
+        	<li>
+            <h4>Web Design/Development</h4>
+              <ul>
+                <li>Creating new digital content for the library: including web-based tutorials,
+                digitally reformatted collections, subject specific pathfinders, and other digital library services. </li>
+                <li>Developing dynamic, evolving, user-centered content and services for all IIT Library web sites. Designed and <em>developed the library's first <a href="http://m.library.iit.edu" target="_blank">mobile website</a></em> (summer 2011). </li>
+                <li>Presented at the <em>2011 <a href="http://eduiconf.org/sessions/the-jquery-mobile-website/" target="_blank">edUI conference</a></em> on using a particular mobile development framework. </li>
+                <li>Introducing <em>best practices</em> for web development such as version control, usability studies and process documentation.</li>
+              </ul>
+          </li>
+
+    			<li>
+            <h4>Application Administration</h4>
+              <ul>
+                <li>Recommending digital services and program content through recommendations, literature review, and statistical reports. </li>
+                <li>Implementing and maintaining applications such as open-URL linkages and federated search software.</li>
+              </ul>
+          </li>
+          
+          <li>    
+            <h4>Reference Assistance / Instruction</h4>
+              <ul>
+                <li>Providing assistance for all method of inquiries while staffing the reference desk, including short instruction sessions. </li>
+                <li>Developing instruction sessions on citation management, advanced search techniques and other digital skills using <em>active learning techniques</em>.</li>
+              </ul>
+          </li>
+		</ul>
+    <h3>Reference Librarian</h3>
 	    <p>Illinois Institute of Technology</p>
 
-      <h3>Web Design/Development</h3>
-        <ul class="columnBlock">
-          <li>Creating new digital content for the library: including web-based tutorials,
-          digitally reformatted collections, subject specific pathfinders, and other digital library services. </li>
-          <li>Developing dynamic, evolving, user-centered content and services for all IIT Library web sites. Designed and <em>developed the library's first <a href="http://m.library.iit.edu" target="_blank">mobile website</a></em> (summer 2011). </li>
-          <li>Presented at the <em>2011 <a href="http://eduiconf.org/sessions/the-jquery-mobile-website/" target="_blank">edUI conference</a></em> on using a particular mobile development framework. </li>
-          <li>Introducing <em>best practices</em> for web development such as version control, usability studies and process documentation.</li>
-        </ul>
-
-      <h3>Application Administration</h3>
-        <ul class="columnBlock">
-          <li>Recommending digital services and program content through recommendations, literature review, and statistical reports. </li>
-          <li>Implementing and maintaining applications such as open-URL linkages and federated search software.</li>
-        </ul>
-
-      <h3>Reference Assistance / Instruction</h3>
-        <ul class="columnBlock">
-          <li>Providing assistance for all method of inquiries while staffing the reference desk, including short instruction sessions. </li>
-          <li>Developing instruction sessions on citation management, advanced search techniques and other digital skills using <em>active learning techniques</em>.</li>
-        </ul>
-
-    <h2>Reference Librarian</h2>
-	    <p>Illinois Institute of Technology</p>
-
-      <h3>Subject Liaison / Instruction</h3>
+      <h4>Subject Liaison / Instruction</h4>
         <ul class="columnBlock">
 	        <li>Supported research at Stuart School of Business and the Institute of Design as subject liaison. </li>
       </ul>
 
-    <h3>Instruction</h3>
+    <h4>Instruction</h4>
     <ul class="columnBlock">
     <li><em>Designed instruction sessions</em> in conjunction with class projects.</li>
     </ul>
 
-    <h3>Reference Assistance</h3>
+    <h4>Reference Assistance</h4>
       <ul class="columnBlock">
 	      <li>Provided assistance for all method of inquiries while staffing the reference desk, including short instruction sessions.</li>	     
       </ul>
 
-    <h3>Collection Development</h3>
+    <h4>Collection Development</h4>
       <ul class="columnBlock">
         <li>Researched and purchased items for monograph resources for business and design subject areas.Also made recommendations for serial purchases in these areas.</li>
         <li>Relocated and weeded items to maintain library collection for business and design areas.</li>
       </ul>
 
-    <h2>Implementation Librarian </h2>
+    <h3>Implementation Librarian </h3>
       <p>Ex Libris Group</p>
       
-      <h3>Customer Management</h3>
+      <h4>Customer Management</h4>
         <ul class="columnBlock">
           <li>Coordinated software installations and upgrades for the Primo discovery and delivery application. </li>
           <li>Communicated issues and requests to the members of the Primo team for resolution and possible 
@@ -70,107 +79,105 @@
           <li>Configured environment and and provided <em>first line support</em> during the implementation cycle.</li>
         </ul>
 
-      <h3>Data Analysis</h3>
+      <h4>Data Analysis</h4>
         <ul class="columnBlock">
           <li>Wrote data map customizations to correct for a variety of display issues.</li>
           <li>Provided analysis of future software needs and current defects to address.</li>
         </ul>
 
-      <h3>Training/Documentation</h3>
+      <h4>Training/Documentation</h4>
         <ul class="columnBlock">
           <li>Developed customized web-based and face-to-face instructional programs using active learning techniques. </li>
           <li>Reviewed end user documentation for clarity and accuracy. </li>
         </ul>
       
-    <h2>Quality Assurance Analyst</h2>
+    <h3>Quality Assurance Analyst</h3>
 	    <p>Endeavor Information Systems</p>
 
-      <h3>Software Testing</h3>
+      <h4>Software Testing</h4>
         <ul class="columnBlock">
           <li>System tested Voyager ILS and Meridian/ENCompass digital content management products.</li> 
           <li>Maintained test environments, including creating test data.</li>
         </ul>
       
-      <h3>Field Testing</h3>
+      <h4>Field Testing</h4>
         <ul class="columnBlock">
           <li>Organized customer test sites and testing process.</li> 
           <li>Provided customer support and acted as liaison between customers and development staff.</li>
         </ul>
       
-      <h3>Test Automation/Training</h3>
+      <h4>Test Automation/Training</h4>
         <ul class="columnBlock">
           <li>Improved test automation methods, <em>refactored code into functional libraries</em> and cross-trained team members in 
           programming methodologies.</li>
           <li>Performed tool analysis comparisons for future performance testing goals.</li>
         </ul>
 
-    <h2>Senior Technical Consultant</h2>
+    <h3>Senior Technical Consultant</h3>
       <p>11th Hour, Inc.</p>
       
-      <h3>Software Testing</h3>
+      <h4>Software Testing</h4>
         <ul class="columnBlock">
           <li>Developed structured testing environments and managed people and processes involved. </li>
           <li>Designed and conducted performance tests to discover application latencies before release.</li>
       </ul>
       
-      <h3>Technical Writing</h3>
+      <h4>Technical Writing</h4>
         <ul class="columnBlock">
 	        <li>Created in-house user manuals and training documents.</li>
         </ul>
       
-      <h3>Training/Supervision</h3>
+      <h4>Training/Supervision</h4>
         <ul class="columnBlock">
           <li>Conducted standard training classes as well developed customized classes.</li>
           <li>Co-wrote and presented a <em>conference paper for an audience of 100+ attendees</em>.</li>
           <li>Supervised teams of 5-10 people, directing activities and outlining work.</li>
         </ul>
       
-      <h3>Website Design</h3>
+      <h4>Website Design</h4>
         <ul class="columnBlock">
 	        <li>Designed, developed and maintained two company websites.</li>
         </ul>
       
-      <h3>Graphic Design</h3>
+      <h4>Graphic Design</h4>
         <ul class="columnBlock">
 	        <li>Designed marketing materials such as <em>logos, business cards and brochures</em>.</li>
         </ul>
-</section>
-<section>
-  <h1>Technical Skills</h1>
-    <h2>Software</h2>
-      <h3>Library Systems</h3>
+
+  <h2>Technical Skills</h2>
+    <h3>Software</h3>
+      <h4>Library Systems</h4>
         <ul class="columnBlock">
 	        <li>Primo Discovery + Delivery, Voyager ILS, Meridian ERM, SFX, MetaLib federated search, ContentDM, ENCompass digital repository</li>
         </ul>
 
-      <h3>Software Development</h3>
+      <h4>Software Development</h4>
         <ul class="columnBlock">
 	        <li>Dreamweaver, BBEdit, NetBeans, <em>GitHub</em></li>
         </ul>
 
-      <h3>General</h3>
+      <h4>General</h4>
         <ul class="columnBlock">
 	        <li>Microsoft Office (including macro and VBA programming)</li>
         </ul>
       
-      <h3>Graphic Design</h3>
+      <h4>Graphic Design</h4>
         <ul class="columnBlock">
 	        <li>Adobe Creative Suite (Photoshop, Illustrator, InDesign)</li>
         </ul>
 
-    <h2>Operating Systems</h2>
+    <h3>Operating Systems</h3>
       <ul class="columnBlock">
 	      <li>Windows 7/XP/NT, UNIX, Linux, Mac OSX</li>
       </ul>
 
-    <h2>Programming Languages</h2>
+    <h3>Programming Languages</h3>
       <ul class="columnBlock">
 	      <li>HTML, PHP, <em>JavaScript/jQuery, Ruby on Rails,</em> MySQL, Perl</li>
       </ul>
 
-</section>
-<section>
-  <h1>Education</h1>
+
+  <h2>Education</h2>
    
     <ul>
       <li>Master Certificate in <em>Web Design &amp; Application Development</em> (2012)</li>
@@ -181,9 +188,8 @@
       </li>
       <li>Bachelor of Science, Eastern Michigan University, Ypsilanti, MI</li>
     </ul>
-</section>
-<section>  
-  <h1>Activities / Interests</h1>
+ 
+  <h2>Activities / Interests</h2>
     <ul class="columnBlock">
       <li>Active member of ALA, RUSA, and LITA</li>
       <li><em>Photography</em> (fine art, portraiture and event photography)</li>
