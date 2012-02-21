8a569df7154517c3b4b112c3206579c001091061
Christine McClure <camcclure@gmail.com>
2012-02-21 09:15:19 -0600

    added events UL for no-JS browsers

--- events/index.php
+++ events/index.php
@@ -19,11 +19,38 @@
   
   <h2>Literature For All of Us</h2>
  	<h3>Honor Thy Mother Luncheon, 2011</h3>
-   <div id="gallery1" class="photoSwipe">Photo galleries require JavaScript to be enabled.</div>
+   <div id="gallery1" class="photoSwipe">
+    <ul>
+      <li><img src="/img/events/IMG_1008.jpg" alt="Photo of event site." /></li>
+      <li><img src="/img/events/IMG_1002.jpg" alt="Photo of centerpiece flower arrangement." /></li>
+      <li><img src="/img/events/IMG_1143.jpg" alt="Photo of an upright bass misician playing in the reception area." /></li>
+      <li><img src="/img/events/IMG_1216.jpg" alt="Photo of registration table volunteers checking in attendees." /></li>
+      <li><img src="/img/events/IMG_1231.jpg" alt="Photo of two attendees chatting during the reception." /></li>
+      <li><img src="/img/events/LFAOU-DPP-05.jpg" alt="Photo of luncheon speaker." /></li>
+      <li><img src="/img/events/IMG_1459.jpg" alt="Photo of luncheon audience listening attently to speaker." /></li>
+      <li><img src="/img/events/IMG_1492.jpg" alt="Photo of founder, author Jane Hamilton and her mother." /></li>
+      <li><img src="/img/events/IMG_1043.jpg" alt="Photo of teen book group participants receiving gifts." /></li>
+      <li><img src="/img/events/IMG_1572.jpg" alt="Photo of volunteers staffing the book signing table, selling books." /></li>
+      <li><img src="/img/events/IMG_1566.jpg" alt="Photo of author Jane Hamilton signing books." /></li>
+    </ul>   
+   </div>
 
   <h2>District 57 Education Foundation</h2>
  	<h3>Live Auction, 2010</h3>
-  <div id="gallery2" class="photoSwipe">Photo galleries require JavaScript to be enabled.</div>
+  <div id="gallery2" class="photoSwipe">
+    <ul>
+      <li><img src="/img/events/2010_District_57_-73.jpg" alt="Photo of several women in cocktail attire at a silent auction table." /></li>
+      <li><img src="/img/events/2010_District_57_-291.jpg" alt="Photo of a dessert table." /></li>
+      <li><img src="/img/events/2010_District_57_-140.jpg" alt="Photo of a raffle drawing." /></li>
+      <li><img src="/img/events/2010_District_57_-125.jpg" alt="Photo of woman holding an Oscar-type stauette award." /></li>
+      <li><img src="/img/events/2010_District_57_-280.jpg" alt="Photo of three women wearing prop hats and boas." /></li>
+      <li><img src="/img/events/2010_District_57_-46.jpg" alt="Photo of two attendees talking." /></li>
+      <li><img src="/img/events/2010_District_57_-216.jpg" alt="Photo of a group of attendees." /></li>
+      <li><img src="/img/events/2010_District_57_-240.jpg" alt="Photo of woman in cocktail attire holding up an auction paddle to bid during the live auction." /></li>
+      <li><img src="/img/events/2010_District_57_-411.jpg" alt="Photo of two women in cocktail attire, dancing." /></li>
+      <li><img src="/img/events/2010_District_57_-242.jpg" alt="Photo of woman laughing heartily during the live auction with the auctioneer behind her." /></li>
+    </ul>  
+  </div>
 </section> 
 
 <?php include '../includes/footer.html'; ?>
