3168a1f14ab077669808082caa5332de3537789e
Christine McClure <camcclure@gmail.com>
2012-02-23 16:27:17 -0600

    changed pricing and added new column style that will always be 2 cols

--- pricing/index.php
+++ pricing/index.php
@@ -11,30 +11,21 @@
 <?php include '../includes/nav.html'; ?>
 <section>
 	
-	<h1 class="top">Pricing</h1>
+	<h1>Pricing</h1>
 
-	<h2 class="top">Event Photography</h2>
 
-	<ul class="twoColumnBlock">
-  <li>
+	<div class="columnEven">
+		<h2>Event Photography</h2>
 		<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
-  </li>
-    <li>
       <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
-    </li>
-  </ul>
+  </div>
 
+	<div class="columnEven">
 	<h2>Portraiture</h2>
-
-	<ul class="twoColumnBlock">
-  	<li>
 			<p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
-  	</li>
-		<li>
     	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.
 After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
-		</li>
-	</ul>
+	</div>
 </section>
 
 <?php include '../includes/footer.html'; ?>
