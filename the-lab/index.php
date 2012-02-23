787b469e91c58205ee93f5d9b9daec242a52ddd1
Christine McClure <camcclure@gmail.com>
2012-02-23 16:29:40 -0600

    used new column design

--- the-lab/index.php
+++ the-lab/index.php
@@ -13,18 +13,17 @@
     <h1 class="top">The Lab</h1>
     <p>What's on the back burner?</p>
 
-         <ul class="twoColumnBlock">
-         	<li>
-          	<h2>New Home Page</h2>
-            <p>Once I put my new site live, I realized that I hated the layout (of course). I had spent so much time 
-    thinking about responsive-ness and mobile-first that I hadn't paid enough attention to the design. 
-    So here is the <a href="http://dev.christinemcclure.com">new home page</a> I'm envisioning.</p></li>
+		<div class="columnEven">
+	    <h2>New Home Page</h2>
+      <p>Once I put my new site live, I realized that I hated the layout (of course). I had spent so much time 
+      thinking about responsive-ness and mobile-first that I hadn't paid enough attention to the design. 
+      So here is the <a href="http://dev.christinemcclure.com">new home page</a> I'm envisioning.</p>
+    </div>  	
 
-					<li>
-            <h2>Ruby on Rails</h2>
-            <p>I've been learning a bit about the Ruby programming language using the Rails framework. It's slow-going because it's such a terse language, but you can see a small example of an application I'm currently developing at <a href="http://electric-ice-8535.heroku.com/">the Heroku site.</a></p>
-            </li>
-            </ul>
+		<div class="columnEven">
+      <h2>Ruby on Rails</h2>
+      <p>I've been learning a bit about the Ruby programming language using the Rails framework. It's slow-going because it's such a terse language, but you can see a small example of an application I'm currently developing at <a href="http://electric-ice-8535.heroku.com/">the Heroku site.</a></p>
+		</div>
 </section>    
 
 <?php include '../includes/footer.html'; ?>
