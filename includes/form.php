0ef86faf24ec9a5c439af06c295cdff133bafee5
Christine McClure <camcclure@gmail.com>
2012-02-27 17:50:25 -0600

    form not working. took out styling

--- includes/form.php
+++ includes/form.php
@@ -1,12 +1,12 @@
     <div class="contactMessage">
 			<?php echo $feedback; ?>  
      </div>
-      <div id="showContactForm" class="<?php echo $cssclass; ?> formContainer"> <!--showForm to start-->	
-      <form method="post" action=<?php echo $formAction; ?> id="contactForm">
-    	  <input type="text" name="name" id="name" placeholder="Name"/>
-  	    <input type="email" name="email" id="email" placeholder="E-mail address" required/>
-  	    <textarea name="message" id="message"  placeholder="Your message here" required></textarea>
-	      <input class="button" type="submit" name="submit" value="Send" id="submitButton" />
-	      <input type="hidden" name="submitted" value="TRUE" />
-  	  </form>
+      <div id="showContactForm" class="<?php echo $cssclass; ?>">  <!-- showForm to start -->	
+        <form method="post" action=<?php echo $formAction; ?> id="contactForm">
+          <input type="text" name="name" id="name" placeholder="Name"/>
+          <input type="email" name="email" id="email" placeholder="E-mail address" required/>
+          <textarea name="message" id="message"  placeholder="Your message here" required></textarea>
+          <input class="button" type="submit" name="submit" value="Send" id="submitButton" />
+          <input type="hidden" name="submitted" value="TRUE" />
+        </form>
       </div>
