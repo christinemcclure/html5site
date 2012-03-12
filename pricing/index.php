4ecc06b573c5aa57c9f08245c73f6dae06da2923
Christine McClure <camcclure@gmail.com>
2012-03-12 16:12:10 -0500

    changed image resultion deliverable for pricing

--- pricing/index.php
+++ pricing/index.php
@@ -15,7 +15,7 @@
 	<div class="columnEven">
 		<h2>Event Photography</h2>
 		<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
-      <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
+      <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 96ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
 		<p>Need a wedding photographer? Contact my good friend, <a href="http://www.davesuarez.com/">Dave Suarez</a>.</p>
   </div>
 
@@ -23,7 +23,7 @@
 	<h2>Portraiture</h2>
 			<p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
     	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.
-After the event, you will receive a CD of web-quality images (600x400 pixels at 100ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
+After the event, you will receive a CD of web-quality images (600x400 pixels at 96ppi). Print-quality images can be <a href="http://christinemcclure.smugmug.com/">ordered online</a> for an additional cost.</p>
 	</div>
 </section>
 
