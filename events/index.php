0142acd8ca91b83a525b5da691a12541d3110836
Christine McClure <camcclure@gmail.com>
2012-06-26 22:04:38 -0500

    matched image names in file to lightroom names

--- events/index.php
+++ events/index.php
@@ -14,20 +14,20 @@
   <h2>Literature For All of Us</h2>
  	<h3>Honor Thy Mother Luncheon, 2011</h3>
     <ul class="print">
-      <li><img src="/img/events/lfaou-2011-14.jpg" alt="Photo of event site." /></li>
-      <li><img src="/img/events/lfaou-2011-12.jpg" alt="Photo of centerpiece flower arrangement." /></li>
-      <li><img src="/img/events/lfaou-2011-13.jpg" alt="Photo of an upright bass misician playing in the reception area." /></li>
-      <li><img src="/img/events/lfaou-2011-4.jpg" alt="Photo of registration table volunteers checking in attendees." /></li>
-      <li><img src="/img/events/lfaou-2011-5.jpg" alt="Photo of two attendees chatting during the reception." /></li>
-      <li><img src="/img/events/lfaou-2011-6.jpg" alt="Photo of luncheon speaker." /></li>
-      <li><img src="/img/events/lfaou-2011-7.jpg" alt="Photo of luncheon audience listening attently to speaker." /></li>
-      <li><img src="/img/events/lfaou-2011-8.jpg" alt="Photo of founder, author Jane Hamilton and her mother." /></li>
-      <li><img src="/img/events/lfaou-2011-11.jpg" alt="Photo of teen book group participants receiving gifts." /></li>
-      <li><img src="/img/events/lfaou-2011-2.jpg" alt="Photo of of a woman signing up for a raffle." /></li>
-      <li><img src="/img/events/lfaou-2011-1.jpg" alt="Photo of two women, event organizers." /></li>
-      <li><img src="/img/events/lfaou-2011-3.jpg" alt="Photo of smilingwoman, holding a raffle basket." /></li>
-      <li><img src="/img/events/lfaou-2011-10.jpg" alt="Photo of volunteers staffing the book signing table, selling books." /></li>
-      <li><img src="/img/events/lfaou-2011-9.jpg" alt="Photo of author Jane Hamilton signing books." /></li>
+      <li><img src="/img/events/2011LFAOU-16.jpg" alt="Photo of event site." /></li>
+      <li><img src="/img/events/2011LFAOU-10.jpg" alt="Photo of centerpiece flower arrangement." /></li>
+      <li><img src="/img/events/2011LFAOU-128.jpg" alt="Photo of an upright bass misician playing in the reception area." /></li>
+      <li><img src="/img/events/2011LFAOU-194.jpg" alt="Photo of registration table volunteers checking in attendees." /></li>
+      <li><img src="/img/events/2011LFAOU-207.jpg" alt="Photo of two attendees chatting during the reception." /></li>
+      <li><img src="/img/events/2011LFAOU-308.jpg" alt="Photo of luncheon speaker." /></li>
+      <li><img src="/img/events/2011LFAOU-416.jpg" alt="Photo of luncheon audience listening attently to speaker." /></li>
+      <li><img src="/img/events/2011LFAOU-449.jpg" alt="Photo of founder, author Jane Hamilton and her mother." /></li>
+      <li><img src="/img/events/2011LFAOU-49.jpg" alt="Photo of teen book group participants receiving gifts." /></li>
+      <li><img src="/img/events/2011LFAOU-105.jpg" alt="Photo of a woman signing up for a raffle." /></li>
+      <li><img src="/img/events/2011LFAOU-90.jpg" alt="Photo of two women, event organizers." /></li>
+      <li><img src="/img/events/2011LFAOU-176.jpg" alt="Photo of smiling woman, holding a raffle basket." /></li>
+      <li><img src="/img/events/2011LFAOU-529.jpg" alt="Photo of volunteers staffing the book signing table, selling books." /></li>
+      <li><img src="/img/events/2011LFAOU-518.jpg" alt="Photo of author Jane Hamilton signing books." /></li>
     </ul>   
    <div id="gallery1" class="photoSwipe">
    </div>
@@ -35,16 +35,17 @@
   <h2>District 57 Education Foundation</h2>
  	<h3>Live Auction, 2010</h3>
     <ul class="print">
-      <li><img src="/img/events/district57-2.jpg" alt="Photo of several women in cocktail attire at a silent auction table." /></li>
-      <li><img src="/img/events/district57-10.jpg" alt="Photo of a dessert table." /></li>
-      <li><img src="/img/events/district57-9.jpg" alt="Photo of a raffle drawing." /></li>
-      <li><img src="/img/events/district57-8.jpg" alt="Photo of woman holding an Oscar-type stauette award." /></li>
-      <li><img src="/img/events/district57-7.jpg" alt="Photo of three women wearing prop hats and boas." /></li>
-      <li><img src="/img/events/district57-1.jpg" alt="Photo of two attendees talking." /></li>
-      <li><img src="/img/events/district57-3.jpg" alt="Photo of a group of attendees." /></li>
-      <li><img src="/img/events/district57-5.jpg" alt="Photo of woman in cocktail attire holding up an auction paddle to bid during the live auction." /></li>
+      <li><img src="/img/events/2010District57-54.jpg" alt="Photo of several women in cocktail attire at a silent auction table." /></li>
+      <li><img src="/img/events/2010District57-223.jpg" alt="Photo of a dessert table." /></li>
+      <li><img src="/img/events/2010District57-110.jpg" alt="Photo of a raffle drawing." /></li>
+      <li><img src="/img/events/2010District57-97.jpg" alt="Photo of woman holding an Oscar-type stauette award." /></li>
+      <li><img src="/img/events/2010District57-210.jpg" alt="Photo of three women wearing prop hats and boas." /></li>
+      <li><img src="/img/events/2010District57-35.jpg" alt="Photo of two attendees talking." /></li>
+      <li><img src="/img/events/2010District57-167.jpg" alt="Photo of a group of attendees." /></li>
+      <li><img src="/img/events/2010District57-184.jpg" alt="Photo of woman in cocktail attire holding up an auction paddle to bid during the live auction." /></li>
       <li><img src="/img/events/district57-6.jpg" alt="Photo of two women in cocktail attire, dancing." /></li>
-      <li><img src="/img/events/district57-4.jpg" alt="Photo of woman laughing heartily during the live auction with the auctioneer behind her." /></li>
+      <li><img src="/img/events/2010District57-185.jpg" alt="Photo of woman laughing heartily during the live auction with the auctioneer behind her." /></li>
+      <li><img src="/img/events/2010District57-320.jpg", alt="Photo of two women in cocktail attire, dancing." /></li>
     </ul>  
   <div id="gallery2" class="photoSwipe">
   </div>
@@ -59,20 +60,20 @@
 				var gallery2;		
 				gallery1 = PhotoSwipe.attach(
 					[
-						{ url: '/img/events/lfaou-2011-14.jpg', alt:'Photo of venue from outside.'},
-						{ url: '/img/events/lfaou-2011-12.jpg', alt:'Photo of centerpiece flower arrangement.'},
-						{ url: '/img/events/lfaou-2011-13.jpg', alt:'Photo of an upright bass musician playing in the reception area.'},
-						{ url: '/img/events/lfaou-2011-4.jpg', alt:'Photo of registration table volunteers checking in attendees.'},
-						{ url: '/img/events/lfaou-2011-5.jpg', alt: 'Photo of two attendees chatting during the reception.'},
-						{ url: '/img/events/lfaou-2011-6.jpg', alt:'Photo of luncheon speaker.'},
-						{ url: '/img/events/lfaou-2011-7.jpg', alt:'Photo of luncheon audience listening attently to speaker.'},
-						{ url: '/img/events/lfaou-2011-8.jpg', alt:'Photo of founder, author Jane Hamilton and her mother.'},
-						{ url: '/img/events/lfaou-2011-11.jpg', alt:'Photo of teen book group participants receiving gifts.'},
-      			{ url: '/img/events/lfaou-2011-2.jpg', alt:'Photo of of a woman signing up for a raffle.'},
-						{ url: '/img/events/lfaou-2011-1.jpg', alt:'Photo of two women, event organizers.'},
-      			{ url: '/img/events/lfaou-2011-3.jpg', alt:'Photo of smilingwoman, holding a raffle basket.'},
-						{ url: '/img/events/lfaou-2011-10.jpg', alt:'Photo of volunteers staffing the book signing table, selling books.'},
-						{ url: '/img/events/lfaou-2011-9.jpg', alt:'Photo of author Jane Hamilton signing books.'}
+						{ url: '/img/events/2011LFAOU-16.jpg', alt:'Photo of venue from outside.'},
+						{ url: '/img/events/2011LFAOU-10.jpg', alt:'Photo of centerpiece flower arrangement.'},
+						{ url: '/img/events/2011LFAOU-128.jpg', alt:'Photo of an upright bass musician playing in the reception area.'},
+						{ url: '/img/events/2011LFAOU-194.jpg', alt:'Photo of registration table volunteers checking in attendees.'},
+						{ url: '/img/events/2011LFAOU-207.jpg', alt: 'Photo of two attendees chatting during the reception.'},
+						{ url: '/img/events/2011LFAOU-308.jpg', alt:'Photo of luncheon speaker.'},
+						{ url: '/img/events/2011LFAOU-416.jpg', alt:'Photo of luncheon audience listening attently to speaker.'},
+						{ url: '/img/events/2011LFAOU-449.jpg', alt:'Photo of founder, author Jane Hamilton and her mother.'},
+						{ url: '/img/events/2011LFAOU-49.jpg', alt:'Photo of teen book group participants receiving gifts.'},
+      			{ url: '/img/events/2011LFAOU-105.jpg', alt:'Photo of a woman signing up for a raffle.'},
+						{ url: '/img/events/2011LFAOU-90.jpg', alt:'Photo of two women, event organizers.'},
+      			{ url: '/img/events/2011LFAOU-176.jpg', alt:'Photo of smiling woman, holding a raffle basket.'},
+						{ url: '/img/events/2011LFAOU-529.jpg', alt:'Photo of volunteers staffing the book signing table, selling books.'},
+						{ url: '/img/events/2011LFAOU-518.jpg', alt:'Photo of author Jane Hamilton signing books.'}
 					],
 					{
 						target: window.document.querySelectorAll('#gallery1')[0],
@@ -86,16 +87,16 @@
 				);
 				gallery2 = PhotoSwipe.attach(
 					[
-						{ url: '/img/events/district57-2.jpg', alt:'Photo of several women in cocktail attire at a silent auction table.'},
-						{ url: '/img/events/district57-3.jpg', alt:'Photo of a group of attendees.'},
-						{ url: '/img/events/district57-10.jpg', alt:'Photo of a dessert table.'},
-						{ url: '/img/events/district57-1.jpg', alt:'Photo of two attendees talking.'},
-						{ url: '/img/events/district57-5.jpg', alt:'Photo of woman in cocktail attire holding up an auction paddle to bid during the live auction.'},
-						{ url: '/img/events/district57-4.jpg', alt:'Photo of woman laughing heartily during the live auction with the auctioneer behind her.'},						
-						{ url: '/img/events/district57-9.jpg', alt:'Photo of a raffle drawing.'},
-						{ url: '/img/events/district57-8.jpg', alt:'Photo of woman holding an Oscar-type stauette award.'},
-						{ url: '/img/events/district57-6.jpg', alt:'Photo of two women in cocktail attire, dancing.'},
-						{ url: '/img/events/district57-7.jpg', alt:'Photo of three women wearing prop hats and boas.'}
+						{ url: '/img/events/2010District57-54.jpg', alt:'Photo of several women in cocktail attire at a silent auction table.'},
+						{ url: '/img/events/2010District57-167.jpg', alt:'Photo of a group of attendees.'},
+						{ url: '/img/events/2010District57-223.jpg', alt:'Photo of a dessert table.'},
+						{ url: '/img/events/2010District57-35.jpg', alt:'Photo of two attendees talking.'},
+						{ url: '/img/events/2010District57-184.jpg', alt:'Photo of woman in cocktail attire holding up an auction paddle to bid during the live auction.'},
+						{ url: '/img/events/2010District57-185.jpg', alt:'Photo of woman laughing heartily during the live auction with the auctioneer behind her.'},						
+						{ url: '/img/events/2010District57-110.jpg', alt:'Photo of a raffle drawing.'},
+						{ url: '/img/events/2010District57-97.jpg', alt:'Photo of woman holding an Oscar-type stauette award.'},
+						{ url: '/img/events/2010District57-320.jpg', alt:'Photo of two women in cocktail attire, dancing.'},
+						{ url: '/img/events/2010District57-210.jpg', alt:'Photo of three women wearing prop hats and boas.'}
 					],
 					{
 						target: window.document.querySelectorAll('#gallery2')[0],
