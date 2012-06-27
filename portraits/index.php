56613f210f6a346793e22299b0e8becdcee98d6b
Christine McClure <camcclure@gmail.com>
2012-06-26 22:29:23 -0500

    changed portrait image names to match lightroom and updated resolution

--- portraits/index.php
+++ portraits/index.php
@@ -11,11 +11,11 @@
 <section>
 	<h1 class="top">Portraiture</h1>
 		<ul class="print">
-			<li><p>Nadia's first photo shoot</p><img src="/img/portraits/baby.jpg" alt="Photo of an 8-month old baby in a garden." /></li>
-			<li><p>Family portrait day</p><img src="/img/portraits/family.jpg" alt="Photo of a a multi-racial family in a garden." /></li>
+			<li><p>Nadia's first photo shoot</p><img src="/img/portraits/2010DrabramsVisit-44.jpg" alt="Photo of an 8-month old baby in a garden." /></li>
+			<li><p>Family portrait day</p><img src="/img/portraits/2010DrabramsVisit-23.jpg" alt="Photo of a a multi-racial family in a garden." /></li>
 			<li><p>Gerry Doyle, "From the Depths" book jacket shoot</p><img src="/img/portraits/gd_book_02.jpg" alt="Black and white photo of author Gerry Doyle holding a beer, with the back of a woman\'s head in the foreground." /></li>
-			<li><p>Ariana's second birthday</p><img src="/img/portraits/birthday.jpg" alt="Photo of a two-year-old girl eating a cookie with powdered sugar on her face." /></li>
-			<li><p>Nadia's first swim</p><img src="/img/portraits/pool.jpg" alt="Photo of a father holding his baby daughter in a swimming pool." /></li>      
+			<li><p>Ariana's second birthday</p><img src="/img/portraits/2011AriannaBirthday-2.jpg" alt="Photo of a two-year-old girl eating a cookie with powdered sugar on her face." /></li>
+			<li><p>Nadia's first swim</p><img src="/img/portraits/KaraGerryWedding-52.jpg" alt="Photo of a father holding his baby daughter in a swimming pool." /></li>      
 			<li><p>Singer, Literature for all of Us benefit at Cary's Lounge, Chicago IL</p><img src="/img/portraits/2009ThisisHell-LFAOU115.jpg" alt="Black and white photo of a smiling singer, head turned to the side." /></li>
 			<li><p>Lety, Greek Independence Day Parade participant, Chicago IL</p><img src="/img/portraits/Lety_Greek_Parade-337.jpg" alt="Young girl in colorful traditional Greek headscarf." /></li>
 		</ul>
@@ -31,19 +31,19 @@
 				var gallery1;		
 				gallery1 = PhotoSwipe.attach(
 					[
-						{ url: '/img/portraits/baby.jpg', 
+						{ url: '/img/portraits/2010DrabramsVisit-44.jpg', 
 								alt:'Photo of an 8-month old baby in a garden."',
 								caption: 'Nadia\'s first photo shoot'},
-						{ url: '/img/portraits/family.jpg', 
+						{ url: '/img/portraits/2010DrabramsVisit-23.jpg', 
 								alt:'Photo of a a multi-racial family in a garden.',
 								caption: 'Family portrait day'},
 						{ url: '/img/portraits/gd_book_02.jpg', 
 								alt:'Black and white photo of author Gerry Doyle holding a beer, with the back of a woman\'s head in the foreground.',
 								caption:'Gerry Doyle, "From the Depths" book jacket shoot'},
-						{ url: '/img/portraits/birthday.jpg', 
+						{ url: '/img/portraits/2011AriannaBirthday-2.jpg', 
 								alt:'Photo of a two-year-old girl eating a cookie with powdered sugar on her face.',
 								caption: 'Ariana\'s second birthday'},						
-						{ url: '/img/portraits/pool.jpg', 
+						{ url: '/img/portraits/KaraGerryWedding-52.jpg', 
 								alt:'Photo of a father holding his baby daughter in a swimming pool.',
 								caption: 'Nadia\'s first swim.'},
 						{ url: '/img/portraits/2009ThisisHell-LFAOU115.jpg', 
