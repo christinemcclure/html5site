dc593e66f058383b079388637af972594bc0e73a
Christine McClure <camcclure@gmail.com>
2013-05-01 16:58:01 -0500

    changed to three-column layout and added online ording link

--- pricing/index.php
+++ pricing/index.php
@@ -11,19 +11,25 @@
 <section>
 	
 	<h1>Pricing</h1>
-
-	<div class="columnEven">
+ 
+	<div class="columnBlock">
 		<h2>Event Photography</h2>
 		<p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
-      <p>After the event, you will receive a collection of high-resolution photos (up to 4MB per picture). Prints, merchandise, and photo downloads will also be available online for family and friends to purchase.</p>
+      <p>After the event, you will receive a collection of high-resolution photos (up to 4MB per picture).</p>
 		<p>Need a wedding photographer? Contact my good friend, <a href="http://www.davesuarez.com/">Dave Suarez</a>.</p>
   </div>
 
-	<div class="columnEven">
+	<div class="columnBlock">
 	<h2>Portraiture</h2>
 			<p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
     	<p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes. After the event, you will receive a collection of high-resolution photos (up to 4MB per picture). Prints, merchandise, and photo downloads will also be available online</a> for family and friends to purchase.</p>
 	</div>
+
+  <div class="columnBlock">
+	<h2>Ordering Prints</h2>
+			<p>Prints, merchandise, and photo downloads are <a href="http://christinemcclure.zenfolio.com">available online</a> for family and friends to purchase.</p>
+	</div>
+
 </section>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.html"); ?>
