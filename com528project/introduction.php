fe37be680186ee5bdf6f823ea50afb98f52ac56a
Christine McClure <camcclure@gmail.com>
2013-09-03 21:16:29 -0500

    added first two assignments to intro

--- com528project/introduction.php
+++ com528project/introduction.php
@@ -2,7 +2,9 @@
 <body id="words">
   <? include "nav.html"; ?>
   <div class="container">
-    <h1>About Me as a Designer...</h1>
+
+    <h1>Introduction</h1>
+    <h2>About Me as a Designer...</h2>
     <p>
         I'm a librarian. And while the nature of my work in digital services means that I rarely ever touch a book, I still exhibit many of
         the profession's classic tendencies. My CDs are alphabetized; I edit work-related emails and Facebook posts with equal fervor, and I favor the rule-of-three
@@ -22,6 +24,34 @@
     <p>
       I'm OK with that. 
     </p>
+
+    <h2>The elements of this portfolio</h2>
+    <p>This portfolio consists of various assignments from a Document Design class I took as part of the <a href="http://www.iit.edu/psych/hum/programs/grad/iarc.shtml">Information
+        Architecture</a> curriculum at Illinois Institute of Technology. Here's a brief description of each assignment included here:
+    </p>
+    <ul>
+      <li>
+        <a href="draw-letters.php">Drawing Letterforms</a>: recreate--by hand drawing or using graphic software, the following letters: "A," "a," and "<em>a</em>--as shown in the Adobe Garamond font."
+      </li>
+      <li>
+        <a href="words.php">Words</a>: Use two of the following words and convey the design concepts of space and meaning by modifying only the font size, spacing and/or placement of the letters.
+        Adapted from an exercise in one of the <a href="http://www.amazon.com/Thinking-Type-2nd-revised-expanded/dp/1568989695/ref=sr_1_1?s=books&ie=UTF8&qid=1378260924&sr=1-1&keywords=thinking+with+type">
+          course textbooks</a>.
+        <ul>
+          <li>compression</li>
+          <li>transition</li>
+          <li>contraction</li>
+          <li>addition</li>
+          <li>subtraction</li>
+          <li>disruption</li>
+          <li>repetition</li>
+          <li>elimination</li>
+          <li>migration</li>
+          <li>expansion</li>
+        </ul>
+      </li>
+    </ul>
+
   </div>
 
 </body>
