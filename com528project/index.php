cc63fa28882452336bc1ea564be321181b69f5aa
Christine McClure <camcclure@gmail.com>
2013-09-03 20:22:31 -0500

    fixed bad body and html tags

--- com528project/index.php
+++ com528project/index.php
@@ -1,7 +1,10 @@
 <? include "header.html"; ?>
+<body id="home">
+  <div class="container">
+    <h1>Christine McClure - Portfolio</h1>
+    <p>(Image or logo placeholder)</p>
+    <p><a href="toc.php">Table of Contents</a></p>
+  </div>
 
-<h1>Christine McClure - Portfolio</h1>
-<p>(Image or logo placeholder)</p>
-<p><a href="toc.php">Table of Contents</a></p>
-
-<? include "footer.html"; ?>
+</body>
+</html>
