0cbfc2e727ad643df5ff3aaff209a2c00f3c1ca4
Christine McClure <camcclure@gmail.com>
2013-09-03 20:01:03 -0500

    added cover page and sep. ToC per instructions

--- com528project/index.php
+++ com528project/index.php
@@ -1,5 +1,6 @@
 <? include "header.html" ?>
 <body id="main">
-<h1>Portfolio</h1>
-  <? include "nav.html" ?>
+<h1>Christine McClure - Portfolio</h1>
+<p>(Image or logo placeholder)</p>
+<p><a href="toc.php">Table of Contents</a></p>
 </body>
---
+++ com528project/toc.php
@@ -0,0 +1,5 @@
+<? include "header.html" ?>
+<body id="main">
+<h1>Portfolio</h1>
+  <? include "nav.html" ?>
+</body>
