810e37cf8d4498f80cc5ff4aac36fe00cae8bbc7
Christine McClure <camcclure@gmail.com>
2013-09-03 20:32:58 -0500

    initial introduction

--- com528project/introduction.php
+++ com528project/introduction.php
@@ -3,7 +3,26 @@
   <? include "nav.html"; ?>
   <div class="container">
     <h1>About Me as a Designer...</h1>
-
+    <p>
+        I'm a librarian. And while the nature of my work in digital services means that I rarely ever touch a book, I still exhibit many of
+        the profession's classic tendencies.
+      My CDs are alphabetized; I edit work-related emails and Facebook posts with equal fervor, and I favor the rule-of-three 
+      in my writing.
+    </p>
+    <p>
+      My designs tend to be very precise. It's not that I don't ever color outside the lines, I do. But when I do, I take care to convey the idea
+      that I know <em>exactly where those lines are, dammit</em>, and so this is intentional.
+    </p>
+    <p>
+      I took a drawing class a few years ago.
+      The professor's comment on my final project was:
+      <blockquote>
+        I love this--it's so obsessive!
+      </blockquote>
+    </p>
+    <p>
+      I'm OK with that. 
+    </p>
   </div>
 
 </body>
