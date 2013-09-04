cc4c1bb2b3fe7c835d5148746051f1135d246583
Christine McClure <camcclure@gmail.com>
2013-09-03 21:11:17 -0500

    changed title of draw letters

--- com528project/draw-letters.php
+++ com528project/draw-letters.php
@@ -2,7 +2,7 @@
 <body id="words">
   <? include "nav.html"; ?>
   <div class="container">
-    <h1>Draw Letters</h1>
+    <h1>Drawing Letters</h1>
     <img src="images/draw-letters.jpg" />
   </div>
 
