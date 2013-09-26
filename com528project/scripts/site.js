9716bd93540767446454bb9ddf7946dddda178f2
Christine McClure <camcclure@gmail.com>
2013-09-26 12:30:19 -0500

    added basic viewport height and width checking for use later on

--- com528project/scripts/site.js
+++ com528project/scripts/site.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
   initBook=function(ele){
+    // Account for varying window sizes
+    var winHeight = $(window).height();
+    var winWidth = $(window).width();
+    if (winHeight < 800){
+      winHeight = '100%';
+    }
+    else {
+      winHeight = '800px'; // change these setting when font size is determined. 
+    }                      // Also include making font size smaller via a class
+    if (winWidth < 1100){
+      winWidth = '100%';
+    }
+    else {
+      winWidth = '1200px';
+    }
+    
+    // Initialize booklet object
     $(ele).booklet({
         closed: true,
-        width: '90%',
-        height: '80%',
+        width: winWidth,
+        height: winHeight,
         hash: true
     });
   }
