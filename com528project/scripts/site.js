e9af9dea69f797b85a1a22e6a06359fdfe59160c
Christine McClure <camcclure@gmail.com>
2013-12-13 15:47:22 -0600

    back down to 900px min width

--- com528project/scripts/site.js
+++ com528project/scripts/site.js
@@ -8,7 +8,7 @@ var tocImages = new Array ('intro','letters','words','text','plp','symmAsymm',
     // Account for varying window sizes
     var winHeight = $(window).height();
     var winWidth = $(window).width();
-    var minWin = 1200;
+    var minWin = 900;
     var aspectRatio = Math.round((winWidth/winHeight)*100)/100;
     var limit = 2.38
 //    alert('width: '+ winWidth + ' height: '+winHeight +' aspect ratio: ' + aspectRatio);
